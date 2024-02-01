4a0aae0dfee0f3211ff5a7104f6838b892628b02
satuk1 <szymi2666@gmail.com>
2024-02-01 09:45:12 +0100

    31.01 (01)

--- Crosfit/crosfat/forms.py
+++ Crosfit/crosfat/forms.py
@@ -12,3 +12,21 @@ class EditProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'password']
+class CustomUserChangeForm(UserChangeForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'password')
+        help_texts = {
+            'password': None,  # To usunie tekst pomocy
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserChangeForm, self).__init__(*args, **kwargs)
+        self.fields['password'].help_text = None
+        # Usuń informacje o algorytmie hashowania
+        if 'instance' in kwargs:
+            self.fields['password'].required = False
+            self.fields['password'].widget = forms.PasswordInput()
+            self.fields['password'].help_text = 'Wprowadź nowe hasło lub pozostaw puste, aby zachować obecne.'
